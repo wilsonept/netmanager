d732e2cbf88e20e0580354b6feb9325292739d25
wilson <wilson@dev>
2022-07-07 21:58:59 +0300

    Руссифицировал класс DataRequired

--- forms.py
+++ forms.py
@@ -1,14 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, StopValidation
 
 """Файл с формами приложения."""
 
 
 # ------ Формы ---------------------------------------------------------
 
+class MyDataRequired(DataRequired):
+    """Полный аналог суперкласса, но на русском."""
+    def __call__(self, form, field):
+        if field.data and (not isinstance(field.data, str) or field.data.strip()):
+            return
+
+        if self.message is None:
+            message = field.gettext("Это поле необходимо.")
+        else:
+            message = self.message
+
+        field.errors[:] = []
+        raise StopValidation(message)
+
+
 class LoginForm(FlaskForm):
     """Форма авторизации."""
-    name = StringField(label="Логин", validators=[DataRequired()])
-    passwd = PasswordField(label="Пароль", validators=[DataRequired()])
-    submit = SubmitField(label="Submit")
+    login = StringField(label="Логин", validators=[MyDataRequired()])
+    passwd = PasswordField(label="Пароль", validators=[MyDataRequired()])
+    submit = SubmitField(label="Войти")
+
+
+class LoginForm(FlaskForm):
+    """Форма администратора."""
+    login = StringField(label="Логин", validators=[MyDataRequired()])
+    passwd = PasswordField(label="Пароль", validators=[MyDataRequired()])
+    submit = SubmitField(label="Войти")
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import redirect, url_for, render_template, request, flash
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, login_required, logout_user
 
 from application import app, LOGIN_MANAGER as log_man
 from models import User
@@ -15,13 +15,21 @@ def load_user(user_id):
 
 # ------- Маршруты приложения ------------------------------------------
 
-@app.route("/", methods=["GET", "POST"])
-@app.route("/home", methods=["GET", "POST"])
+@app.route("/") 
+@app.route("/home")
 def home():
     if not current_user.is_anonymous and current_user.id == 1: 
-        return render_template("admin.html")
+        return redirect(url_for("admin"))
     else:
         return redirect(url_for("login"))
+
+
+@app.route("/admin", methods=["GET", "POST"])
+@login_required
+def admin():
+    if current_user.is_authenticated:
+        return render_template("admin.html")
+    return render_template("login.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -30,7 +38,7 @@ def login():
     if request.method == "POST":
         if form.validate_on_submit():
             # Аутентификация пользователя.
-            user = User.query.filter_by(name=form.name.data).first()
+            user = User.query.filter_by(name=form.login.data).first()
 
             if user:
                 # Авторизация пользователя.
@@ -40,13 +48,15 @@ def login():
                     return redirect(url_for("home"))
 
             flash("Не верный логин или пароль, повторите попытку.")
-    return render_template("login.html", form=form)
+    return render_template("login.html", form=form, errors=form.errors)
 
 
 @app.route("/logout")
+@login_required
 def logout():
-    # TODO Выход из пользователя.
-    pass
+    if current_user.is_authenticated:
+        logout_user()
+    return redirect(url_for("login"))
 
 
 # ------- Маршруты API -------------------------------------------------
